619b9a53a25d72bff7ede1ac4b2a2baa3d56e88f
Ugo-42 <42.ugo.42@gmail.com>
2024-12-19 14:50:11 +0100

    corrected main.c

--- get_next_line_main/main.c
+++ get_next_line_main/main.c
@@ -19,19 +19,19 @@ int	main(int argc, char **argv)
 	char	*line;
 	int		fd;
 
-	if (argc == 2)
+	if (argc == 1)
+		fd = 0;
+	else
 	{
 		fd = open(argv[1], O_RDONLY);
 		if (fd == -1)
 			return (-1);
 	}
-	if (argc == 1)
-		fd = 0;
+	line = get_next_line(fd);
+	printf("%s", line);
 	while (line)
 	{
 		line = get_next_line(fd);
-		if (!line)
-			return (-1);
 		printf("%s", line);
 		free(line);
 	}
